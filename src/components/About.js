4255efe8676c94b4ba47bd39b0063cf46248994e
nickming <nickmingyang@gmail.com>
2020-08-25 12:04:17 -0500

    Added About.js with Lorem, ipsum

---
+++ src/components/About.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-12 text-center p-5">
+            <h2>The REWYND STORY</h2>
+          </div>
+          <div className="col-2"></div>
+          <div className="col-8 text-center mb-5">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet
+              facere eligendi unde ex repellendus reiciendis at, molestiae
+              debitis officia, modi tempora nihil sunt, quibusdam eos in
+              voluptates obcaecati! Blanditiis, repudiandae.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet
+              facere eligendi unde ex repellendus reiciendis at, molestiae
+              debitis officia, modi tempora nihil sunt, quibusdam eos in
+              voluptates obcaecati! Blanditiis, repudiandae.
+            </p>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet
+              facere eligendi unde ex repellendus reiciendis at, molestiae
+              debitis officia, modi tempora nihil sunt, quibusdam eos in
+              voluptates obcaecati! Blanditiis, repudiandae.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default About;
